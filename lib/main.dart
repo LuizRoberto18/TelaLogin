a70375cbccec138ce90737346873ccf9f08ceb72
Luiz Roberto <passageiro1829@gmail.com>
2021-10-21 20:56:15 -0300

    tela de login completa

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:exemplo_tela_login/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dog Life',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
